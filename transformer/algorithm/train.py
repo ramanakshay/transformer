a54945feb528f7241a52522c17ccc57d29f18718
ramanakshay <ar10021161@gmail.com>
2025-02-11 16:37:45 -0500

    updated trainer

--- transformer/algorithm/train.py
+++ transformer/algorithm/train.py
@@ -18,6 +18,14 @@ to avoid zero raising to negative power.
     )
 
 
+class TrainState:
+    """Track number of steps, examples, and tokens processed"""
+
+    step: int = 0  # Steps in the current epoch
+    accum_step: int = 0  # Number of gradient accumulation steps
+    samples: int = 0  # total # of examples used
+    tokens: int = 0  # total # of tokens processed
+
 class Trainer(object):
     def __init__(self, data, model, config):
         self.data = data
